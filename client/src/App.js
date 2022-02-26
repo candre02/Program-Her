533c99c566839e999da8dcca81dc53eb3177f821
Crystal Andre <81876258+candre02@users.noreply.github.com>
2022-02-26 15:47:28 -0600

    edited app.js file, added the imported files, routes for navbar exact path

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+// import files
+import React from 'react';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Services from './pages/Services';
+import Comments from './pages/Comments';
+import ProgramHer from './pages/ProgramHer';
+import Nav from './components/Nav';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import './App.css';
 
+// app function
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={ProgramHer} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/services" component={Services} />
+        <Route exact path="/comments" component={Comments} />
+        <Route exact path="/contact" component={Contact} />
+      </Switch>
+    </Router>
   );
 }
 
+// export app
 export default App;
