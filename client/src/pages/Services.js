904b7d29672e4d23241ae52f6c640be71b19518c
Crystal Andre <81876258+candre02@users.noreply.github.com>
2022-02-26 12:27:59 -0600

    created services file, added 4 headings: services, networking,job searches, and reasources

---
+++ client/src/pages/Services.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Services() {
+  return (
+    <section>
+      <div>
+        <h1>Services</h1>
+      </div>
+      <div>
+        <h2>Networking</h2>
+      </div>
+      <div>
+        <h3>Job Searches</h3>
+      </div>
+      <div>
+        <h4>Reasources</h4>
+      </div>
+    </section>
+  )
+}
+
+export default Services
