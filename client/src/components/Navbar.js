b52068bb1fa81d0a61f596f2c621a9cfffdb8b13
krod2016 <katrinajrodriguez1993@gmail.com>
2022-02-26 11:05:08 -0600

    updated navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav, Container, Modal, Tab } from 'react-bootstrap';
 import SignUpForm from './SignupForm';
 import LoginForm from './LoginForm';
+import About from '../Pages/About';
+import Comments from '../Pages/Comments';
 
 // import Auth from '../utils/auth';
 
