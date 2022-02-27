010dc7330a72ce4c0e1464667bcca80a21ac4453
Crystal Andre <81876258+candre02@users.noreply.github.com>
2022-02-26 21:13:21 -0600

    added chakra to app file and installed the package

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 // import files
-import React from 'react';
+import * as React from 'react';
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Services from './pages/Services';
@@ -7,11 +7,14 @@ import Comments from './pages/Comments';
 import ProgramHer from './pages/ProgramHer';
 import Nav from './components/Nav';
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
-
+// import 'chakraProvider' component
+import { ChakraProvider } from '@chakra-ui/react';
 
 // app function
-function App() {
+function App({ Component }) {
   return (
+    <ChakraProvider>
+      <Component />
     <Router>
       <Nav />
       <Switch>
@@ -22,6 +25,7 @@ function App() {
         <Route exact path="/contact" component={Contact} />
       </Switch>
     </Router>
+  </ChakraProvider>  
   );
 }
 
