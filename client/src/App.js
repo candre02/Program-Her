8f128ffdd11cd697e15cd24c2159c490038508dc
Crystal Andre <81876258+candre02@users.noreply.github.com>
2022-02-27 00:09:07 -0600

    server file: moved app.get('*',(req, res) to be inside lines 42-48 also added comment that it is for heroku, added chakraprovider to app file, moved lines 18-23 to be inside contactform function vs outside

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 // import files
 import * as React from 'react';
+// import pages
 import About from './pages/About';
 import Contact from './pages/Contact';
 import Services from './pages/Services';
 import Comments from './pages/Comments';
 import ProgramHer from './pages/ProgramHer';
-
+// import react-router-dom
 import { Route, BrowserRouter as Router } from "react-router-dom";
 // import 'chakraProvider' component
 import { ChakraProvider } from '@chakra-ui/react';
-
-
+// import components
 import Navbar from "./components/Navbar"; 
 import Footer from "./components/Footer";
 
@@ -22,13 +22,11 @@ function App({Component}) {
        <Component />
     <Router>
       <Navbar />
-      
         <Route exact path="/" component={ProgramHer} />
         <Route exact path="/about" component={About} />
         <Route exact path="/services" component={Services} />
         <Route exact path="/comments" component={Comments} />
         <Route exact path="/contact" component={Contact} />
-      
     </Router>
     </ChakraProvider>   
     <div className="App">
--- server/server.js
+++ server/server.js
@@ -39,15 +39,13 @@ startServer()
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
+// Serve up static assets- this is for heroku
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 }
-
-
 
 db.once('open', () => {
   app.listen(PORT, () => {
